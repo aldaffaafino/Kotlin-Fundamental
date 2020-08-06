00b70679309e40cf370a35b226747f94fb496b03
aldaffaafino <aldaffaafino@gmail.com>
2020-08-06 20:00:39 +0700

    first commit

---
+++ src/main/kotlin/Data Types.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val firstWord = "SMK "
+    val middleWord = "Telkom "
+    val lastWord = "Purwokerto"
+    val valueA: Int= 43
+    val valueB = 43
+    println(firstWord + middleWord + lastWord)
+    print("Luas persegi panjang " + (valueA + valueB))
+}
---
+++ src/main/kotlin/Hello Kotlin.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val name = "Al Daffa Ageng Firstarano"
+    val hobby = "Vidiografi"
+
+    print("Nama saya ")
+    println(name)
+    print("Hobi saya ")
+    println(hobby)
+}
